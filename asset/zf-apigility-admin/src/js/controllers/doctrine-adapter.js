624efb2358dd7187c71f2c47953c82ee1d685d2a
Dominic Watson <domwatson@live.co.uk>
2014-02-06 01:22:21 +0100

    Sqlite now takes a Path parameter and will show different options when selected. The Select dropdown is also now populated when selected. MongoODM `options` parameter was removed as it expects an array and username was renamed to user for the ODM

--- asset/zf-apigility-admin/src/js/controllers/doctrine-adapter.js
+++ asset/zf-apigility-admin/src/js/controllers/doctrine-adapter.js
@@ -1,72 +1,92 @@
-(function(_, $) {'use strict';
+(function(_, $) {
+    'use strict';
 
-angular.module('ag-admin').controller(
-    'DoctrineAdapterController',
-    ['$scope', '$location', 'flash', 'DoctrineAdapterResource', function ($scope, $location, flash, DoctrineAdapterResource) {
-        $scope.doctrineAdapters = [];
-        $scope.showNewDoctrineoctrinedapterForm = false;
+    angular.module('ag-admin').controller(
+        'DoctrineAdapterController', ['$scope', '$location', 'flash', 'DoctrineAdapterResource',
+            function($scope, $location, flash, DoctrineAdapterResource) {
+                $scope.doctrineAdapters = [];
+                $scope.showNewDoctrineoctrinedapterForm = false;
 
-        $scope.resetForm = function () {
-            $scope.showNewDoctrineAdapterForm = false;
-            $scope.adapterName = '';
-            $scope.driver      = '';
-            $scope.database    = '';
-            $scope.username    = '';
-            $scope.password    = '';
-            $scope.hostname    = 'localhost';
-            $scope.port        = '';
-        };
-
-        function updateDoctrineAdapters(force) {
-            $scope.doctrineAdapters = [];
-            DoctrineAdapterResource.fetch({force: force}).then(function (doctrineAdapters) {
-                $scope.$apply(function () {
-                    $scope.doctrineAdapters = _.pluck(doctrineAdapters.embedded.doctrine_adapter, 'props');
-                });
-            });
-        }
-        updateDoctrineAdapters(false);
-
-        $scope.saveDoctrineAdapter = function (index) {
-            var doctrineAdapter = $scope.doctrineAdapters[index];
-            
-            var options;
-            if(doctrineAdapter.adapter_name != 'odm_default') {
-                options = {
-                    driverClass     :  doctrineAdapter.driver,
-                    params: {
-                        dbname      :  doctrineAdapter.params.dbname,
-                        user        :  doctrineAdapter.params.user,
-                        password    :  doctrineAdapter.params.password,
-                        host        :  doctrineAdapter.params.host,
-                        port        :  doctrineAdapter.params.port
-                    } 
+                $scope.ui = {
+                    ormAdapters: {
+                        'Doctrine\\DBAL\\Driver\\DrizzlePDOMySql\\Driver': 'DrizzlePDOMySql',
+                        'Doctrine\\DBAL\\Driver\\IbmDb2\\DB2Driver': 'IbmDb2',
+                        'Doctrine\\DBAL\\Driver\\Mysqli\\Driver': 'Mysqli',
+                        'Doctrine\\DBAL\\Driver\\OCI8\\Driver': 'Oci8',
+                        'Doctrine\\DBAL\\Driver\\PDOIbm\\Driver': 'Pdo_IBM',
+                        'Doctrine\\DBAL\\Driver\\PDOMySql\\Driver': 'Pdo_Mysql',
+                        'Doctrine\\DBAL\\Driver\\PDOOracle\\Driver': 'Pdo_Oracle',
+                        'Doctrine\\DBAL\\Driver\\PDOPgSql\\Driver': 'Pdo_Pgsql',
+                        'Doctrine\\DBAL\\Driver\\PDOSqlite\\Driver': 'Pdo_Sqlite',
+                        'Doctrine\\DBAL\\Driver\\PDOSqlsrv\\Driver': 'Pdo_Sqlsrv',
+                        'Doctrine\\DBAL\\Driver\\Sqlsrv\\Driver': 'Sqlsrv'
+                    }  
                 };
-            } else {
-                options = {
-                    dbname          : doctrineAdapter.dbname,
-                    connectionString: doctrineAdapter.connectionString,
-                    username        : doctrineAdapter.user,
-                    password        : doctrineAdapter.password,
-                    port            : doctrineAdapter.port,
-                    server          : doctrineAdapter.server,
-                    options         : doctrineAdapter.options
+
+                $scope.resetForm = function() {
+                    $scope.showNewDoctrineAdapterForm = false;
+                    $scope.adapterName = '';
+                    $scope.driverClass = '';
+                    $scope.database = '';
+                    $scope.username = '';
+                    $scope.password = '';
+                    $scope.hostname = 'localhost';
+                    $scope.port = '';
+                };
+
+                function updateDoctrineAdapters(force) {
+                    $scope.doctrineAdapters = [];
+                    DoctrineAdapterResource.fetch({
+                        force: force
+                    }).then(function(doctrineAdapters) {
+                        $scope.$apply(function() {
+                            $scope.doctrineAdapters = _.pluck(doctrineAdapters.embedded.doctrine_adapter, 'props');
+                        });
+                    });
+                }
+                updateDoctrineAdapters(false);
+
+                $scope.saveDoctrineAdapter = function(index) {
+                    var doctrineAdapter = $scope.doctrineAdapters[index];
+
+                    var options;
+                    if (doctrineAdapter.adapter_name != 'odm_default') {
+                        options = {
+                            driverClass: doctrineAdapter.driverClass,
+                            params: {
+                                path: doctrineAdapter.params.path,
+                                dbname: doctrineAdapter.params.dbname,
+                                user: doctrineAdapter.params.user,
+                                password: doctrineAdapter.params.password,
+                                host: doctrineAdapter.params.host,
+                                port: doctrineAdapter.params.port
+                            }
+                        };
+                    } else {
+                        options = {
+                            dbname: doctrineAdapter.dbname,
+                            connectionString: doctrineAdapter.connectionString,
+                            user: doctrineAdapter.user,
+                            password: doctrineAdapter.password,
+                            port: doctrineAdapter.port,
+                            server: doctrineAdapter.server
+                        };
+                    }
+                    DoctrineAdapterResource.saveAdapter(doctrineAdapter.adapter_name, options).then(function(doctrineAdapter) {
+                        flash.success = 'Doctrine adapter ' + doctrineAdapter.adapter_name + ' updated';
+                        updateDoctrineAdapters(true);
+                    });
+                };
+
+                $scope.removeDoctrineAdapter = function(adapter_name) {
+                    DoctrineAdapterResource.removeAdapter(adapter_name).then(function() {
+                        flash.success = 'Doctrine adapter ' + adapter_name + ' reset';
+                        updateDoctrineAdapters(true);
+                        $scope.deleteDoctrineAdapter = false;
+                    });
                 };
             }
-            DoctrineAdapterResource.saveAdapter(doctrineAdapter.adapter_name, options).then(function (doctrineAdapter) {
-                flash.success = 'Doctrine adapter ' + doctrineAdapter.adapter_name + ' updated';
-                updateDoctrineAdapters(true);
-            });
-        };
-
-        $scope.removeDoctrineAdapter = function (adapter_name) {
-            DoctrineAdapterResource.removeAdapter(adapter_name).then(function () {
-                flash.success = 'Doctrine adapter ' + adapter_name + ' reset';
-                updateDoctrineAdapters(true);
-                $scope.deleteDoctrineAdapter = false;
-            });
-        };
-    }]
-);
+        ]
+    );
 
 })(_, $);
